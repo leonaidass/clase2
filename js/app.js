21bb626d11fd6eca32e3c7cd00f792c2698a6aa8
Leonardo Esquivel <leo22esquivel@gmail.com>
2021-11-10 22:03:49 -0300

    ejercicio2

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var app = new Vue({
     data: {
       message: 'Hello Vue!',
       mensaje1: 'hola mundo',
-      mensaje2:"mensaje 2"
+      mensaje2:"mensaje 2",
+      valorInput:"",
+      valor1:"https://cdn.shopify.com/s/files/1/0229/0839/files/bancos_de_imagenes_gratis.jpg?v=1630420628&width=1024"
     }
   })
